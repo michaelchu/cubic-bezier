c957c98a409cb65f5c1d2bb126951e8eea7a0820
Bobby Juncosa <bobbyjuncosa@gmail.com>
2019-10-24 13:54:38 -0400

    test: added :ease_out_quad

--- test/cubic_bezier_test.exs
+++ test/cubic_bezier_test.exs
@@ -14,6 +14,10 @@ defmodule CubicBezierTest do
       assert Float.ceil(solve(0.25, :ease_in_quad, 400), 5) == 0.07429
       assert Float.ceil(solve(0.50, :ease_in_quad, 400), 5) == 0.25599
       assert Float.ceil(solve(0.75, :ease_in_quad, 400), 5) == 0.59797
+
+      assert Float.ceil(solve(0.25, :ease_out_quad, 400), 5) == 0.45338
+      assert Float.ceil(solve(0.50, :ease_out_quad, 400), 5) == 0.77133
+      assert Float.ceil(solve(0.75, :ease_out_quad, 400), 5) == 0.93606
     end
 
     test "control_points/1" do
